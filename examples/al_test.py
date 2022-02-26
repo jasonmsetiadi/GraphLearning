8f7b6d0096a99087254bc1537648e9842488291c
jasonmsetiadi <66339366+jasonmsetiadi@users.noreply.github.com>
2022-02-26 17:32:34 -0600

    edit al_test

--- examples/al_test.py
+++ examples/al_test.py
@@ -7,9 +7,9 @@ import sklearn.datasets as datasets
 X,labels = datasets.make_moons(n_samples=500,noise=0.1)
 W = gl.weightmatrix.knn(X,10)
 train_ind = gl.trainsets.generate(labels, rate=5)
-plt.scatter(X[:,0],X[:,1], c=labels)
-plt.scatter(X[train_ind,0],X[train_ind,1], c='r')
-plt.show()
+#plt.scatter(X[:,0],X[:,1], c=labels)
+#plt.scatter(X[train_ind,0],X[train_ind,1], c='r')
+#plt.show()
 
 model = gl.ssl.laplace(W)
 acq = al.model_change_vopt()
@@ -22,10 +22,10 @@ for i in range(10):
     act.update_labeled_data(query_points, query_labels) # update the active_learning object's labeled set
 
     # plot
-    plt.scatter(X[:,0],X[:,1], c=labels)
-    plt.scatter(X[act.current_labeled_set,0],X[act.current_labeled_set,1], c='r')
-    plt.scatter(X[query_points,0],X[query_points,1], c='r', marker='*', s=200, edgecolors='k', linewidths=1.5)
-    plt.show()
-    # print(al.current_labeled_set)
-    # print(al.current_labels)
+    #plt.scatter(X[:,0],X[:,1], c=labels)
+    #plt.scatter(X[act.current_labeled_set,0],X[act.current_labeled_set,1], c='r')
+    #plt.scatter(X[query_points,0],X[query_points,1], c='r', marker='*', s=200, edgecolors='k', linewidths=1.5)
+    #plt.show()
+    print(act.current_labeled_set)
+    print(act.current_labels)
 
