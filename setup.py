c09084d395021ef0d8445f902551adff4ecd26e9
jasonmsetiadi <66339366+jasonmsetiadi@users.noreply.github.com>
2022-03-06 12:15:33 -0600

    original setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="graphlearning", 
-    version="1.1.0",
+    version="1.1.1",
     author="Jeff Calder",
     author_email="jwcalder@umn.edu",
     description="Python package for graph-based clustering and semi-supervised learning",
@@ -14,6 +14,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jwcalder/GraphLearning",
     packages=['graphlearning'],
+    ext_modules=[setuptools.Extension('graphlearning.cextensions', 
+                    sources=[   'c_code/cextensions.c',
+                                'c_code/lp_iterate.c',
+                                'c_code/hjsolvers.c',
+                                'c_code/memory_allocation.c',
+                                'c_code/mnist_benchmark.c',
+                                'c_code/mbo_speedy_volume_preserving.c'],
+                    include_dirs=[numpy.get_include()],
+                    extra_compile_args = ['-Ofast','-std=gnu99'],
+                    extra_link_args = ['-lm'])],
     classifiers=[
                 "Programming Language :: Python :: 3",
                 "License :: OSI Approved :: MIT License",
@@ -24,5 +34,3 @@ setuptools.setup(
                         'matplotlib'],
     python_requires='>=3.6',
 )
-
-
